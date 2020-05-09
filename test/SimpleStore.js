552a72b1066cb3d7d9293aa82aea886daa6a007a
Chris <ctverceles@gmail.com>
2020-05-09 14:22:20 +0800

    update abi contract folder

--- test/SimpleStore.js
+++ test/SimpleStore.js
@@ -1,6 +1,6 @@
-const SimpleStore = artifacts.require('SimpleStore');
+const SimpleStore = artifacts.require('SimpleStore')
 
-contract('SimpleStore', (accounts) => {
+contract('SimpleStore', accounts => {
   let simpleStore
 
   beforeEach(async () => {
@@ -9,14 +9,14 @@ contract('SimpleStore', (accounts) => {
 
   it('Should have an address for SimpleStore', async () => {
     assert(simpleStore.address)
-  });
+  })
 
   it('Should set a value', async () => {
     const newValue = 1
-    const tx = await simpleStore.set(newValue, {from: accounts[0]})
+    const tx = await simpleStore.set(newValue, { from: accounts[0] })
     assert.equal(tx.logs[0].args._value.toNumber(), newValue)
 
-    const result = await simpleStore.get.call({from: accounts[1]})
+    const result = await simpleStore.get.call({ from: accounts[1] })
     assert.equal(result[0], newValue)
     assert.equal(result[1], accounts[1])
   })
--- truffle-config.js
+++ truffle-config.js
@@ -7,25 +7,36 @@ const { sha256 } = require('js-sha256')
 const { CryptoUtils } = require('loom-js')
 const { mnemonicToSeedSync } = require('bip39')
 const fs = require('fs')
-const PrivateKeyProvider = require("truffle-privatekey-provider");
-const EXPECTING_FILE_ERROR = "Expecting either a private key or a mnemonic file. Refer to the README file for more details."
+const PrivateKeyProvider = require('truffle-privatekey-provider')
+const EXPECTING_FILE_ERROR =
+  'Expecting either a private key or a mnemonic file. Refer to the README file for more details.'
 
-
-function getLoomProviderWithPrivateKey(privateKeyPath, chainId, writeUrl, readUrl) {
+function getLoomProviderWithPrivateKey(
+  privateKeyPath,
+  chainId,
+  writeUrl,
+  readUrl
+) {
   const privateKey = readFileSync(privateKeyPath, 'utf-8')
   return new LoomTruffleProvider(chainId, writeUrl, readUrl, privateKey)
 }
 
 function getLoomProviderWithMnemonic(mnemonicPath, chainId, writeUrl, readUrl) {
-  const mnemonic = readFileSync(mnemonicPath, 'utf-8').toString().trim()
+  const mnemonic = readFileSync(mnemonicPath, 'utf-8')
+    .toString()
+    .trim()
   const seed = mnemonicToSeedSync(mnemonic)
-  const privateKeyUint8ArrayFromSeed = CryptoUtils.generatePrivateKeyFromSeed(new Uint8Array(sha256.array(seed)))
-  const privateKeyB64 = CryptoUtils.Uint8ArrayToB64(privateKeyUint8ArrayFromSeed)
+  const privateKeyUint8ArrayFromSeed = CryptoUtils.generatePrivateKeyFromSeed(
+    new Uint8Array(sha256.array(seed))
+  )
+  const privateKeyB64 = CryptoUtils.Uint8ArrayToB64(
+    privateKeyUint8ArrayFromSeed
+  )
   return new LoomTruffleProvider(chainId, writeUrl, readUrl, privateKeyB64)
 }
 
 module.exports = {
-  contracts_build_directory: join(__dirname, './src/contracts'),
+  contracts_build_directory: join(__dirname, '../react-web/src/contracts'),
   compilers: {
     solc: {
       version: '0.5.0'
@@ -33,18 +44,31 @@ module.exports = {
   },
   networks: {
     loom_dapp_chain: {
-      provider: function () {
+      provider: function() {
         const chainId = 'default'
         const writeUrl = 'http://127.0.0.1:46658/rpc'
         const readUrl = 'http://127.0.0.1:46658/query'
         const mnemonicPath = path.join(__dirname, 'loom_mnemonic')
         const privateKeyPath = path.join(__dirname, 'loom_private_key')
         if (fs.existsSync(privateKeyPath)) {
-          const loomTruffleProvider = getLoomProviderWithPrivateKey(privateKeyPath, chainId, writeUrl, readUrl)
-          loomTruffleProvider.createExtraAccountsFromMnemonic("gravity top burden flip student usage spell purchase hundred improve check genre", 10)
+          const loomTruffleProvider = getLoomProviderWithPrivateKey(
+            privateKeyPath,
+            chainId,
+            writeUrl,
+            readUrl
+          )
+          loomTruffleProvider.createExtraAccountsFromMnemonic(
+            'gravity top burden flip student usage spell purchase hundred improve check genre',
+            10
+          )
           return loomTruffleProvider
         } else if (fs.existsSync(mnemonicPath)) {
-          const loomTruffleProvider = getLoomProviderWithMnemonic(mnemonicPath, chainId, writeUrl, readUrl)
+          const loomTruffleProvider = getLoomProviderWithMnemonic(
+            mnemonicPath,
+            chainId,
+            writeUrl,
+            readUrl
+          )
           return loomTruffleProvider
         } else {
           throw new Error(EXPECTING_FILE_ERROR)
@@ -53,17 +77,27 @@ module.exports = {
       network_id: '*'
     },
     loomv2b: {
-      provider: function () {
+      provider: function() {
         const chainId = 'loomv2b'
         const writeUrl = 'http://loomv2b.dappchains.com:46658/rpc'
         const readUrl = 'http://loomv2b.dappchains.com:46658/query'
         const mnemonicPath = path.join(__dirname, 'loomv2b_mnemonic')
         const privateKeyPath = path.join(__dirname, 'loomv2b_pk')
         if (fs.existsSync(privateKeyPath)) {
-          const loomTruffleProvider = getLoomProviderWithPrivateKey(privateKeyPath, chainId, writeUrl, readUrl)
+          const loomTruffleProvider = getLoomProviderWithPrivateKey(
+            privateKeyPath,
+            chainId,
+            writeUrl,
+            readUrl
+          )
           return loomTruffleProvider
         } else if (fs.existsSync(mnemonicPath)) {
-          const loomTruffleProvider = getLoomProviderWithMnemonic(mnemonicPath, chainId, writeUrl, readUrl)
+          const loomTruffleProvider = getLoomProviderWithMnemonic(
+            mnemonicPath,
+            chainId,
+            writeUrl,
+            readUrl
+          )
           return loomTruffleProvider
         } else {
           throw new Error(EXPECTING_FILE_ERROR)
@@ -72,20 +106,33 @@ module.exports = {
       network_id: '12106039541279'
     },
     extdev_plasma_us1: {
-      provider: function () {
+      provider: function() {
         const chainId = 'extdev-plasma-us1'
         const writeUrl = 'http://extdev-plasma-us1.dappchains.com:80/rpc'
         const readUrl = 'http://extdev-plasma-us1.dappchains.com:80/query'
         const mnemonicPath = path.join(__dirname, 'extdev_mnemonic')
         const privateKeyPath = path.join(__dirname, 'extdev_private_key')
         if (fs.existsSync(privateKeyPath)) {
-          const loomTruffleProvider = getLoomProviderWithPrivateKey(privateKeyPath, chainId, writeUrl, readUrl)
+          const loomTruffleProvider = getLoomProviderWithPrivateKey(
+            privateKeyPath,
+            chainId,
+            writeUrl,
+            readUrl
+          )
           // use a dummy mnemonic to create a bunch of accounts we'll use for testing purposes
-          loomTruffleProvider.createExtraAccountsFromMnemonic("gravity top burden flip student usage spell purchase hundred improve check genre", 10)
+          loomTruffleProvider.createExtraAccountsFromMnemonic(
+            'gravity top burden flip student usage spell purchase hundred improve check genre',
+            10
+          )
 
           return loomTruffleProvider
         } else if (fs.existsSync(mnemonicPath)) {
-          const loomTruffleProvider = getLoomProviderWithMnemonic(mnemonicPath, chainId, writeUrl, readUrl)
+          const loomTruffleProvider = getLoomProviderWithMnemonic(
+            mnemonicPath,
+            chainId,
+            writeUrl,
+            readUrl
+          )
           return loomTruffleProvider
         } else {
           throw new Error(EXPECTING_FILE_ERROR)
@@ -94,17 +141,27 @@ module.exports = {
       network_id: '9545242630824'
     },
     loom_mainnet: {
-      provider: function () {
+      provider: function() {
         const chainId = 'default'
         const writeUrl = 'http://plasma.dappchains.com/rpc'
         const readUrl = 'http://plasma.dappchains.com/query'
         const mnemonicPath = path.join(__dirname, 'mainnet_mnemonic')
         const privateKeyPath = path.join(__dirname, 'mainnet_private_key')
         if (fs.existsSync(privateKeyPath)) {
-          const loomTruffleProvider = getLoomProviderWithPrivateKey(privateKeyPath, chainId, writeUrl, readUrl)
+          const loomTruffleProvider = getLoomProviderWithPrivateKey(
+            privateKeyPath,
+            chainId,
+            writeUrl,
+            readUrl
+          )
           return loomTruffleProvider
         } else if (fs.existsSync(mnemonicPath)) {
-          const loomTruffleProvider = getLoomProviderWithMnemonic(mnemonicPath, chainId, writeUrl, readUrl)
+          const loomTruffleProvider = getLoomProviderWithMnemonic(
+            mnemonicPath,
+            chainId,
+            writeUrl,
+            readUrl
+          )
           return loomTruffleProvider
         } else {
           throw new Error(EXPECTING_FILE_ERROR)
@@ -113,18 +170,34 @@ module.exports = {
       network_id: '*'
     },
     rinkeby: {
-      provider: function () {
+      provider: function() {
         if (!process.env.INFURA_API_KEY) {
-          throw new Error("INFURA_API_KEY env var not set")
+          throw new Error('INFURA_API_KEY env var not set')
         }
         const mnemonicPath = path.join(__dirname, 'rinkeby_mnemonic')
         const privateKeyPath = path.join(__dirname, 'rinkeby_private_key')
         if (fs.existsSync(privateKeyPath)) {
-          const privateKey = readFileSync(path.join(__dirname, 'rinkeby_private_key'), 'utf-8')
-          return new PrivateKeyProvider(privateKey, `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`, 0, 10)
+          const privateKey = readFileSync(
+            path.join(__dirname, 'rinkeby_private_key'),
+            'utf-8'
+          )
+          return new PrivateKeyProvider(
+            privateKey,
+            `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+            0,
+            10
+          )
         } else if (fs.existsSync(mnemonicPath)) {
-          const mnemonic = readFileSync(path.join(__dirname, 'rinkeby_mnemonic'), 'utf-8')
-          return new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`, 0, 10)
+          const mnemonic = readFileSync(
+            path.join(__dirname, 'rinkeby_mnemonic'),
+            'utf-8'
+          )
+          return new HDWalletProvider(
+            mnemonic,
+            `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+            0,
+            10
+          )
         } else {
           throw new Error(EXPECTING_FILE_ERROR)
         }
